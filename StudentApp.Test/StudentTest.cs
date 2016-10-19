43779ac1f7a712b8c4fc290b1d6b52159518ae4b
ovini <opo@eXilesoft.com>
2016-10-19 18:16:42 +0530

    Project added

---
+++ StudentApp.Test/StudentTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentApp.Controllers;
+using System.Net.Http;
+using System.Web.Http.Hosting;
+using System.Web.Http;
+using System.Threading.Tasks;
+
+namespace StudentApp.Test
+{
+    [TestClass]
+    public class StudentTest
+    {
+        [TestMethod]
+        public void GetStudent_ShouldReturn_Student()
+        {
+            //Arrange
+            StudentController studentController = new StudentController();
+
+            studentController.Request = new HttpRequestMessage()
+            {
+                Properties = { { HttpPropertyKeys.HttpConfigurationKey, new HttpConfiguration() } }
+            };
+
+
+            //Act
+            var response = studentController.Get();
+            Student student;
+            Assert.IsTrue(response.TryGetContentValue<Student>(out student));
+
+            //Assert
+            Assert.AreEqual(1, student.Id);
+        }
+    }
+}
---
+++ StudentApp/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentApp.Controllers
+{
+   
+    public class StudentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            Student student = new Student { Id = 1, Name = "ABC" };
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, student);
+            return response;
+        }
+    }
+
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
